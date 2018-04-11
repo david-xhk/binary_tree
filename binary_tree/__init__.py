f6a1c0efb4f6bf7443b29e9005dfd3b9fc684cda
han-keong <37938921+han-keong@users.noreply.github.com>
2018-04-11 14:23:30 +0800

    Update __init__.py

--- binary_tree/__init__.py
+++ binary_tree/__init__.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
 """
 Binary tree implementation.
-For a demonstration, run "./binary_tree treestring".
+For a demonstration, run "./tree treestring".
 
 Done by Han Keong
 Created on 10/04/2018 2251 +0800
-Last updated on 11/04/2018 1402 +0800
+Last updated on 11/04/2018 1422 +0800
 """
-
 from node import Node
 from traversals import traverse_pre_order, traverse_in_order, traverse_post_order, traverse_level_order
 from helpers import get_max_depth, get_all_paths
@@ -17,54 +15,3 @@ __author__ = "Han Keong"
 __email__ = "hk997@live.com"
 __version__ = "0.0.1"
 
-if __name__ == "__main__":
-    def listen_lines():
-        while True:
-            line = input("Enter a binary tree:\n")
-            if line:
-                yield line
-            else:
-                print("Goodbye!")
-                return
-    
-    def print_in(*args):
-        print(">>>", *args)
-
-    def print_out(*args, **kwargs):
-        if not any(isinstance(arg, (list, tuple)) for arg in args):
-            print(*args, **kwargs)
-        else:
-            for arg in args:
-                if not any(isinstance(arg, (list, tuple)) for arg in arg):
-                    print(*map(str, arg), sep=", ")
-                else:
-                    print_out(*arg)
-
-    commands = [
-        "treestring",
-        "repr(Node.from_string(treestring))",
-        "list(traverse_pre_order(root))",
-        "list(traverse_in_order(root))",
-        "list(traverse_post_order(root))",
-        "list(traverse_level_order(root))",
-        "get_max_depth(root)",
-        "is_symmetric(root)",
-        "list(get_all_paths(root))",
-        "has_path_sum(root, 22)",
-        "repr(Node.from_in_pre_orders(inorder, preorder))",
-        "repr(Node.from_in_post_orders(inorder, postorder))",
-    ]
-    
-    for treestring in listenLines():
-        root = Node.from_string(treestring)
-        if not root:
-            print("Invalid treestring.")
-            continue
-        preorder = list(traverse_pre_order(root))
-        inorder = list(traverse_in_order(root))
-        postorder = list(traverse_post_order(root))
-        for command in commands:
-            print_in(command)
-            exec(f"print_out({command})")
-        print()
-            
