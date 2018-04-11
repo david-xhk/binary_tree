b504fa3eeffbbad56df6401f4f13911973a47d4e
han-keong <37938921+han-keong@users.noreply.github.com>
2018-04-11 12:50:42 +0800

    Update and rename test_master.py to test_from_string.py

---
+++ tests/test_from_string.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `master` package."""
+
+import pytest
+from binary_tree import Node
+
+
+@pytest.fixture
+def make_tree():
+    """Test from_string constructor."""
+    treestring = "1, 2, 3, 4, 5, null, 6, 7"
+    return Node.from_string(treestring)
+
+
+def test_tree(node):
+    """Check the tree structure."""
+    assert node.value = 1
+    assert node.left.value = 2
+    assert node.right.value = 3
+    assert node.left.left.value = 4
+    assert node.left.right.value = 5
+    assert node.right.right.value = 6
+    assert node.left.left.left.value = 7
+
