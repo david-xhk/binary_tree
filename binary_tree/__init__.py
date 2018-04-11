a3d4bb3b3d579c4e788954fe8a8ed2a2de7a4ffe
han-keong <37938921+han-keong@users.noreply.github.com>
2018-04-11 14:02:55 +0800

    Update __init__.py

--- binary_tree/__init__.py
+++ binary_tree/__init__.py
@@ -5,7 +5,7 @@ For a demonstration, run "./binary_tree treestring".
 
 Done by Han Keong
 Created on 10/04/2018 2251 +0800
-Last updated on 11/04/2018 1333 +0800
+Last updated on 11/04/2018 1402 +0800
 """
 
 from node import Node
@@ -13,8 +13,9 @@ from traversals import traverse_pre_order, traverse_in_order, traverse_post_orde
 from helpers import get_max_depth, get_all_paths
 from misc import is_node, is_symmetric, has_path_sum
 
-__author__ = 'Han Keong'
-__email__ = 'hk997@live.com'
+__author__ = "Han Keong"
+__email__ = "hk997@live.com"
+__version__ = "0.0.2"
 
 if __name__ == "__main__":
     def listen_lines():
