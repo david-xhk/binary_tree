f0806f8378d518ad6865f42b03b3537fb9c8add7
han-keong <37938921+han-keong@users.noreply.github.com>
2018-04-11 13:41:47 +0800

    Update misc.py

--- binary_tree/misc.py
+++ binary_tree/misc.py
@@ -3,7 +3,7 @@ Miscellaneous functions for Node objects.
 
 Done by Han Keong
 Created on 10/04/2018 2239 +0800
-Last updated on 11/04/2018 1335 +0800
+Last updated on 11/04/2018 1341 +0800
 """
 
 
