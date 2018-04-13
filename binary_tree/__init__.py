17d3441b378551eec9b1499762cd33fa396b786a
Han <hk997@live.com>
2018-04-14 02:11:41 +0800

    Rewrote docs.rst

--- binary_tree/__init__.py
+++ binary_tree/__init__.py
@@ -1,14 +1,16 @@
 """binary_tree provides a Node object and useful tools for processing a binary tree data structure
 
 Example:
-    import binary_tree as tree
+    ::
 
-    node = tree.Node.from_string("1,2,,3,4,,5")
-    
-    in_order = [node.value for node in tree.traverse("in", node)]
-    pre_order = [node.value for node in tree.traverse("pre", node)]
-    node2 = Node.from_orders("in-pre", in_order, pre_order)
-    
+        import binary_tree as tree
+
+        node = tree.Node.from_string("1,2,,3,4,,5")
+        
+        in_order = [node.value for node in tree.traverse("in", node)]
+        pre_order = [node.value for node in tree.traverse("pre", node)]
+        node2 = Node.from_orders("in-pre", in_order, pre_order)
+
 >>> node == node2
 True
 """
@@ -22,19 +24,3 @@ del node
 
 from .tree import *
 del tree
-
-# __all__ = [
-#     "Node",
-#     "is_node", 
-#     "is_leaf_node", 
-#     "traverse_pre_order", 
-#     "traverse_in_order", 
-#     "traverse_post_order", 
-#     "traverse_level_order", 
-#     "traverse"
-#     "get_max_depth", 
-#     "get_all_paths",
-#     "is_symmetrical", 
-#     "has_path_sum",
-#     ]
-
--- docs/docs.rst
+++ docs/docs.rst
@@ -1,27 +1,11 @@
 *************
-Documentation
+:mod:`binary_tree`
 *************
 
 This page displays the documentation for the Node class and tree functions in binary_tree.py.
 
-.. currentmodule:: binary_tree
-
-===============
-``binary_tree``
-===============
-
 .. automodule:: binary_tree
-
-====================
-``binary_tree.node``
-====================
-
-.. automodule:: binary_tree.node
     :members:
+    :imported-members: True
 
-====================
-``binary_tree.tree``
-====================
 
-.. automodule:: binary_tree.tree
-    :members:
