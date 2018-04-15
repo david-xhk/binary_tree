ebc3140f59cfb32b5d71a5f8cb16a62eb51b5576
Han <hk997@live.com>
2018-04-15 18:14:12 +0800

    Added and fixed Node.from_orders tests

--- tests/test_binary_tree.py
+++ tests/test_binary_tree.py
@@ -8,7 +8,7 @@ from binary_tree import *
 
 @pytest.fixture
 def node_from_string():
-    """Test from_string constructor."""
+    """Test Node.from_string constructor."""
     tree_string = "1,2,3,4,,5,6"
     return Node.from_string(tree_string)
 
@@ -24,7 +24,7 @@ def test_node_from_string(node_from_string):
 
 @pytest.fixture
 def node_from_in_pre_orders():
-    """Test from_orders (in-pre) constructor."""
+    """Test Node.from_orders (in-pre) constructor."""
     in_order = [4,2,1,5,3,6]
     pre_order = [1,2,4,3,5,6]
     return Node.from_orders("in-pre", in_order, pre_order)
@@ -41,10 +41,10 @@ def test_node_from_in_pre_orders(node_from_in_pre_orders):
 
 @pytest.fixture
 def node_from_in_post_orders():
-    """Test from_orders (in-post) constructor."""
+    """Test Node.from_orders (in-post) constructor."""
     in_order = [4,2,1,5,3,6]
     post_order = [4,2,5,6,3,1]
-    return Node.from_orders("in-post", in_order, pre_order)
+    return Node.from_orders("in-post", in_order, post_order)
 
 def test_node_from_in_post_orders(node_from_in_post_orders):
     """Check the tree structure."""
@@ -55,3 +55,4 @@ def test_node_from_in_post_orders(node_from_in_post_orders):
     assert node.left.right is None
     assert node.right.left.value == 5
     assert node.right.right.value == 6
+
