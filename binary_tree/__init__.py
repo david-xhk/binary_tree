53edf4710d61bb217765b5f81da32a2ca5ed2308
Han <hk997@live.com>
2018-04-17 20:04:22 +0800

    Version 0.1.0

--- binary_tree/__init__.py
+++ binary_tree/__init__.py
@@ -1,37 +1,63 @@
 # -*- coding: utf-8 -*-
 
-"""This package provides a Node object and useful tools for processing a binary tree data structure. 
+"""This package consists of a Node class and functions for processing a binary tree data structure. 
 
 For example::
 
-    from binary_tree import Node, traverse
+    from binary_tree import from_string, from_orders, traverse
 
-    node = Node.from_string("1,2,,3,4,,5")
+    node = tree_from_string("1,2,,3,4,,5")
     
-    in_order = [node.value for node in traverse("in", node)]
-    pre_order = [node.value for node in traverse("pre", node)]
-    node2 = Node.from_orders("in-pre", in_order, pre_order)
+    in_order = list(traverse("in", node))
+    pre_order = list(traverse("pre", node))
+    node2 = from_orders("in-pre", in_order, pre_order)
 
 >>> node == node2
 True
 
-For a demonstration of this module, you can run this in your terminal (for macOS)::
+For demonstrations of this module, you can run the following in your terminal (for macOS)::
 
     $ git clone https://github.com/han-keong/binary_tree.git
-    $ binary_tree/demo/tree.py
+    $ binary_tree/node_demo.py
+    $ binary_tree/tree_demo.py
 """
 
 __author__ = 'Han Keong'
 __email__ = 'hk997@live.com'
-__version__ = '0.0.7'
+__version__ = '0.1.0'
 
 from .node import *
-del node
-
 from .tree import *
-del tree
+from .tools import ensure_type
 
-__all__ = ["Node", "is_node", "is_leaf", "has_path_sum", 
-           "is_symmetrical", "traverse_pre_order", "traverse_in_order",
-           "traverse_post_order", "traverse_level_order", "traverse", 
-           "get_all_paths", "get_max_depth"]
+__all__ = [
+    "Node",
+    "is_node",
+    "is_left",
+    "is_right",
+    "is_leaf",
+    "is_root",
+    "is_orphan",
+    "from_string",
+    "from_orders",
+    "traverse_pre_order",
+    "traverse_in_order",
+    "traverse_post_order",
+    "traverse_level_order",
+    "traverse",
+    "is_symmetrical",
+    "connect_nodes",
+    "get_max_depth",
+    "get_path",
+    "get_all_paths",
+    "has_path_sum",
+    "find_path",
+    "get_lca",
+    ]
+
+ensure_node = ensure_type(root=Node, node=Node, nodes=Node)
+for name in __all__:
+    globals()[name] = ensure_node(globals()[name])
+
+del node, tree, tools, ensure_type, name
+
--- binary_tree/tree.py
+++ binary_tree/tree.py
@@ -2,64 +2,123 @@
 
 """This module contains functions for binary trees."""
 
-from .node import is_node
+from .node import Node, is_node, is_leaf
 
-def has_path_sum(node, value):
-    """Determine if a binary tree contains a root-to-leaf path that adds up
-    to `value`.
+def from_string(tree_string, cls=Node):
+    """Generate a binary tree from a string.
+
+    Instantiates the left child, and then the right child for every node 
+    in each level (level-order).
     
     Args:
-        node (Node): A binary tree root.
-        value: The value to check for.
-
+        tree_string (str): A level-order binary tree traversal, separated
+        by commas.
+        cls (type): The class constructor for the tree. Defaults to Node.
+    
     Returns:
-        ``True`` if a path that adds up to `value` exists, ``False`` otherwise.
+        A newly instantiated Node representing `tree_string`.
+        If `tree_string` has no root value, returns ``None``.
     """
-    for path in get_all_paths(node):
-        total = None
-        for node in path:
-            if total is None:
-                total = node.value
-            else:
-                total += node.value
-        if total == value:
-            return True
-    else:
-        return False
-
-def is_symmetrical(node):
-    """Check for symmetry in a binary tree.
-    
-    Args:
-        node (Node): A binary tree root.
-
-    Return:
-        ``True`` if the binary tree is symmetrical, ``False`` otherwise.
-    """
-    level = [node]
-    while any(level):
-        values = []
+    for char in " []\n'\"":
+        tree_string = tree_string.replace(char, "")
+    values = iter(tree_string.split(","))
+    value = next(values)
+    if value == "":  # Empty root value.
+        return None
+    try:
+        value = int(value)
+    except ValueError:  # value is not a number.
+        pass
+    root = cls(value)
+    level = [root]
+    while level:
         next_level = []
         for node in level:
-            values.append(getattr(node, "value", None))
             for side in ["left", "right"]:
-                next_level.append(getattr(node, side, None))
-        if values != values[::-1]:
-            return False
+                try:
+                    value = next(values)
+                except StopIteration:  # values has been exhausted.
+                    return root
+                if value in ["", "null"]:  # Not a node.
+                    continue
+                try:
+                    value = int(value)
+                except ValueError:  # value is not a number.
+                    pass
+                child = cls(value)
+                setattr(node, side, child)
+                next_level.append(child)
         level = next_level
-    else:
-        return True
+    else:  
+        # next_level is an empty list, so subsequent node values are lost.
+        return root
 
-def traverse_pre_order(node):
-    """Visit the parent, the left child, and then the right child.
+def from_orders(kind, in_order, other_order, cls=Node):
+    """Generate a binary tree from in-order and pre/post-order traversal.
+
+    Recursively instantiates the parent, its left child, and then its 
+    right child (pre-order).
     
     Args:
-        node (Node): A binary tree root.
+        kind (str): Either "in-pre" or "in-post".
+        in_order (list[int, ...]): The in-order traversal of a binary tree.
+        other_order (list[int, ...]): Either the tree's pre-order or 
+            post-order traversal.
+        cls (type): The class constructor for the tree. Defaults to Node.
+
+    Returns:
+        A newly instantiated Node entailing `in_order` and `other_order`.
+        If either arguments are empty, returns ``None``.
+
+    Raises:
+        ValueError: If `in_order` and `other_order` do not correspond or 
+            contain duplicates.
+        KeyError: If `kind` is not one of the accepted keys.
+
+    Note:
+        There cannot be any duplicates in `in_order` and `other_order`.
+    """
+    if kind == "in-pre":
+        def make_node(in_order, other_order):
+            if not in_order or not other_order:
+                return None
+            node = cls(other_order[0])
+            in_slice = in_order[:in_order.index(other_order[0])]
+            other_slice = other_order[1:len(in_slice)+1]
+            node.left = make_node(in_slice, other_slice)
+            in_slice = in_order[in_order.index(other_order[0])+1:]
+            other_slice = other_order[-len(in_slice):]
+            node.right = make_node(in_slice, other_slice)
+            return node
+    elif kind == "in-post":
+        def make_node(in_order, other_order):
+            if not in_order or not other_order:
+                return None
+            node = cls(other_order[-1])
+            in_slice = in_order[:in_order.index(other_order[-1])]
+            other_slice = other_order[:len(in_slice)]
+            node.left = make_node(in_slice, other_slice)
+            in_slice = in_order[in_order.index(other_order[-1])+1:]
+            other_slice = other_order[-len(in_slice)-1:-1]
+            node.right = make_node(in_slice, other_slice)
+            return node    
+    else:
+        raise KeyError("Invalid argument for kind. "
+                       "Expected \"in-pre\" or \"in-post\"")
+    return make_node(in_order, other_order)
+
+def traverse_pre_order(root):
+    """Traverse a binary tree in pre-order.
+
+    Visit the parent, the left child, and then the right child.
+    
+    Args:
+        root (Node): The root node of a binary tree.
 
     Yields:
         Node: A node in the binary tree.
     """
-    queue = [node]
+    queue = [root]
     while queue:
         node = queue.pop()
         if is_node(node):
@@ -69,16 +128,18 @@ def traverse_pre_order(node):
         if is_node(node.left):
             queue.append(node.left)
 
-def traverse_in_order(node):
-    """Visit the left child, the parent, and then the right child.
+def traverse_in_order(root):
+    """Traverse a binary tree in in-order.
+
+    Visit the left child, the parent, and then the right child.
     
     Args:
-        node (Node): A binary tree root.
+        root (Node): The root node of a binary tree.
 
     Yields:
         Node: A node in the binary tree.
     """
-    queue = [node]
+    queue = [root]
     while True:
         while is_node(queue[-1].left):
             queue.append(queue[-1].left)
@@ -92,16 +153,18 @@ def traverse_in_order(node):
         else:
             return
 
-def traverse_post_order(node):
-    """Visit the left and right children, and then the parent.
+def traverse_post_order(root):
+    """Traverse a binary tree in post-order.
+
+    Visit the left child, the right child, and then the parent.
     
     Args:
-        node (Node): A binary tree root.
+        root (Node): The root node of a binary tree.
 
     Yields:
         Node: A node in the binary tree.
     """
-    queue = [node]
+    queue = [root]
     visited = []
     while True:
         while is_node(queue[-1].left):
@@ -112,85 +175,185 @@ def traverse_post_order(node):
                 visited.append(node)
                 queue.append(node.right)
                 break
-            if is_node(node):
-                yield node
+            yield node
             queue.pop()
         else:
             return
 
-def traverse_level_order(node):
-    """Visit each parent in the level, then their left and right children.
+def traverse_level_order(root):
+    """Traverse a binary tree in level-order.
+
+    Visit the left child, and then the right child for each parent in each level.
     
     Args:
-        node (Node): A binary tree root.
+        root (Node): The root node of a binary tree.
 
     Yields:
-        tuple[Node, ...]: A level of nodes in the binary tree.
+        list[Node, ...]: A level of nodes in the binary tree.
     """
-    level = [node]
-    while any(level):
+    level = [root]
+    while level:
+        yield list(level)
         next_level = []
-        nodes = []
         for node in level:
-            if not is_node(node):
-                continue
-            nodes.append(node)
             for side in ["left", "right"]:
-                next_level.append(getattr(node, side))
-        yield tuple(nodes)
+                child = getattr(node, side)
+                if is_node(child):
+                    next_level.append(child)
         level = next_level
 
-def traverse(kind, node):
-    """Dispatch the requested kind of traversal.
+def traverse(root, kind):
+    """Dispatch root to the `kind` traversal.
     
     Args:
+        root (Node): The root node of a binary tree.
         kind (str): "pre" or "in" or "post" or "level".
-        node (Node): A binary tree root.
 
     Returns:
-        The requested traversal generator iterator.
+        The traversal generator iterator for `kind`.
     
     Raises:
         KeyError: If `kind` is not one of the possible options.
     """
     traversal = globals()["traverse_{kind}_order".format(kind=kind)]
-    return traversal(node)
+    return traversal(root)
 
-def get_all_paths(node):
-    """Find every root-to-leaf path in a binary tree.
+def is_symmetrical(root):
+    """Check for symmetry in a binary tree.
     
     Args:
-        node (Node): A binary tree root.
+        root (Node): The root node of a binary tree.
 
-    Yields:
-        tuple[Node, ...]: A copy of every node from the root to a leaf.
+    Returns:
+        ``True`` if the binary tree is symmetrical, ``False`` otherwise.
     """
-    queue = [node]
-    visited = []
-    while True:
-        while is_node(queue[-1].left):
-            queue.append(queue[-1].left)
-        while queue:
-            node = queue[-1]
-            if is_node(node.right):
-                if node not in visited:
-                    visited.append(node)
-                    queue.append(node.right)
-                    break
-            elif not is_node(node.left):
-                yield tuple(queue)
-            queue.pop()
-        else: 
-            return
+    level = [root]
+    while any(level):
+        values = []
+        next_level = []
+        for node in level:
+            values.append(getattr(node, "value", None))
+            for side in ["left", "right"]:
+                next_level.append(getattr(node, side, None))
+        for i in range(len(values)):
+            if values[i] != values[-i-1]:
+                return False
+        level = next_level
+    else:
+        return True
 
-def get_max_depth(node):
+def connect_nodes(root):
+    """Connect the nodes in each level down the tree.
+    
+    Args:
+        root (Node): The root node of a binary tree.
+    """
+    for level in traverse_level_order(root):
+        prev_node, next_node = None, None
+        for i in range(len(level)):
+            level[i].prev, prev_node = prev_node, level[i]
+            level[-i-1].next, next_node = next_node, level[-i-1]
+
+def get_max_depth(root):
     """Calculate the maximum depth of a binary tree.
     
     Args:
-        node (Node): A binary tree root.
+        root (Node): The root node of a binary tree.
 
-    Return:
+    Returns:
         int: The total number of levels of the binary tree.
     """
-    return sum(1 for level in traverse_level_order(node))
+    return sum(1 for level in traverse_level_order(root))
+
+def get_path(node):
+    """Get the path of a node in a binary tree.
+    
+    Args:
+        node (Node): A node in the binary tree.
+
+    Returns:
+        list[Node, ...]: A list of every node from the root to `node`.
+    """    
+    path = [node]
+    parent = node.parent
+    while parent:
+        path.append(parent)
+        parent = parent.parent
+    path.reverse()
+    return path
+
+def get_all_paths(root):
+    """Find every leaf path in a binary tree.
+    
+    Args:
+        root (Node): The root node of a binary tree.
+
+    Yields:
+        list[Node, ...]: A list of every node from the root to a leaf.
+    """
+    for node in traverse_post_order(root):
+        if is_leaf(node):
+            yield get_path(node)
+
+def has_path_sum(root, value):
+    """Determine if there is a path that adds up to `value`.
+    
+    Args:
+        root (Node): The root node of a binary tree.
+        value: The value to check for.
+
+    Returns:
+        ``True`` if a path that adds up to `value` exists, ``False`` otherwise.
+    """
+    for path in get_all_paths(root):
+        total = None
+        for node in path:
+            if total is None:
+                total = node.value
+            else:
+                total += node.value
+        if total == value:
+            return True
+    else:
+        return False
+
+def find_path(root, node):
+    """Find the path of a node in a binary tree.
+    
+    Args:
+        root (Node): The root node of a binary tree.
+        node (Node): A node in the binary tree.
+
+    Returns:
+        list[Node, ...]: A list of every node from `root` to `node`, or ``None`` if `node` is absent in `root`.
+    """
+    for root_node in traverse_post_order(root):
+        if node == root_node:
+            return get_path(root_node)
+
+def get_lca(root, *nodes):
+    """Get the lowest common ancestor of two nodes in a binary tree.
+    
+    Args:
+        root (Node): The root node of a binary tree.
+        *nodes (Node): Various nodes in the binary tree.
+
+    Returns:
+        Node: The lowest common ancestor of `nodes`, or ``None`` if there is no common ancestor.
+    """
+    if len(nodes) < 2:
+        return None
+    paths = []
+    lca_index = None
+    for node in nodes:
+        path = find_path(root, node)
+        max_index = len(path) - 1
+        if lca_index is None or max_index < lca_index:
+            lca_index = max_index
+        paths.append(path)
+    ref_path = paths.pop()
+    for path in paths:
+        while path[lca_index] is not ref_path[lca_index]:
+            lca_index -= 1
+    return ref_path[lca_index]
 
